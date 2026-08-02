2991eb3c4f88dd9971a5a7826163ab7a5e465f39
Yaroslav Pshenichnikov <yaroslav.pshenichnikov@onlyoffice.com>
2026-08-02 12:09:50 +0300

    Fix 16kb allignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.vosk.android)
+    implementation(libs.androidx.graphics.path)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
